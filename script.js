8d1b730d71a791f83b2fcbad72af8578ee1c2f39
Israel Fitsum <fafisru@gmail.com>
2023-01-25 14:37:25 +0300

    Function - Highorder and First-Class functions

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-'use strict';
-
+/* 'use strict';
 const bookings = [];
-
-const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
   const booking = {
     flightNum,
     numPassengers: numPassengers,
@@ -12,5 +14,91 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199 * numP
   bookings.push(booking);
 };
 createBooking('AB12');
-createBooking('AG23',4,1000)
-// console.log(bookings);
+createBooking('AG23', 4, 1000);
+createBooking('AG23', 4);
+createBooking('AG23', 10);
+createBooking('AG23', undefined, 1000);
+
+const flight = 'AG12';
+const jonas = {
+  name: 'Izzy Beluga',
+  passport: 2343434243434,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'SS12';
+  passenger.name = 'Ms.' + passenger.name;
+
+  if (passenger.passport === 2343434243434) {
+    alert('Checked In');
+  } else alert('Wrong Passport');
+  // console.log(flightNum);
+};
+
+//checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000000);
+};
+newPassport(jonas);
+console.log(jonas);
+checkIn(flight, jonas);
+
+
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const capFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// High-order function
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  // console.log(`original function: ${fn}`);
+  console.log(`Transformed to: ${fn(str)}`);
+  console.log(`Transformed By: ${fn.name}`);
+};
+
+// transformer('Javascript is Weird asf 🤓', capFirstWord);
+// transformer('Javascript is Weird asf 🤓', oneWord)
+
+
+//JS uses callbacks all the time
+const highFive = function () {
+  console.log('✋');
+};
+
+document.body.addEventListener('click', highFive);
+['J', 'I', 'A'].forEach(highFive);
+
+let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = numbers.map(function(n) { return n * 2; });
+console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+
+
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Wssup');
+greeterHey('Drake');
+greeterHey('Kanye');
+
+greet('Hey')('Torpa');
+
+//Challenge : - transform to arrow function
+
+const greetArrow = greeting2 => name2 => console.log(`${greeting2} ${name2}`);
+
+greetArrow('Hi')('Travis');
+ */
