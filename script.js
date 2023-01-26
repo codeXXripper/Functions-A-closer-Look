e8fdee914da4414fa0e2a3ffd5828c681521a17e
Israel Fitsum <fafisru@gmail.com>
2023-01-26 21:57:40 +0300

    closures

--- script.js
+++ script.js
@@ -313,6 +313,7 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
  */
 
+/* 
 const once = function () {
   console.log('this will never run again');
 };
@@ -324,3 +325,94 @@ once();
 //Arrow function
 (() => console.log('This will never run again'))();
 
+ */
+
+//Closures
+/* 
+const secBooking = function () {
+  let mysteryVariable = 0;
+
+  return function () {
+    mysteryVariable++;
+    console.log(`${mysteryVariable} users`);
+  };
+};
+
+const reservation = secBooking();
+reservation();
+reservation();
+reservation();
+
+console.dir(reservation) 
+
+
+//Example 1
+
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 7;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+h();
+f();
+
+//Example 2
+
+const gamePlayers = function (n, wait) {
+  const perTeam = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now starting with ${n} all players`);
+    console.log(`There are 3 teams , each with ${perTeam} players`);
+  }, wait * 1000);
+
+  console.log(`Will start playing in ${wait} seconds`);
+};
+
+const perTeam = 22;
+gamePlayers(90, 2);
+*/
+
+/*
+
+Coding Challenge #2
+This is more of a thinking challenge than a coding challenge 🤓
+Your tasks:
+1. Take the IIFE below and at the end of the function, attach an event listener that
+changes the color of the selected h1 element ('header') to blue, each time
+the body element is clicked. Do not select the h1 element again!
+2. And now explain to yourself (or someone around you) why this worked! Take all
+the time you need. Think about when exactly the callback function is executed,
+and what that means for the variables involved in this example.
+(function () {
+const header = document.querySelector('h1');
+header.style.color = 'red';
+})();
+GOOD LUCK 😀
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body');
+  addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
